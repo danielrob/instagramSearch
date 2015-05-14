5a17ce6c597bd9301457f17173ffab5ad7d326a7
Daniel Robinson <djr.self@gmail.com>
2015-05-14 15:03:38 -0400

    add form & submission

--- js/instagramSearcher.js
+++ js/instagramSearcher.js
@@ -1,5 +1,15 @@
 var app = angular.module('instagramSearcher', []);
 
 app.controller('appController', ['$scope', function ($scope) {
-  $scope.helloworld = 'appController says hello world';
+  $scope.search = function(query){
+    $scope.query = undefined;
+    $scope.instasearch.$setPristine();
+    $scope.instasearch.$setUntouched();
+    $scope.query_processing = query;
+    $scope.searching = true;
+    searchInstagram(query);
+  };
+
+  function searchInstagram(query){
+  };
 }])
